c8cdcbd3b436d36db2f1fde3268df1f0639a57a6
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2024-04-21 09:05:23 +0300

    Daily Update

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,8 +1,22 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+logo = logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+print(logo)
 
 def caesar_cipher(direction, received_text, shift_num):
     encrypted_text = ''
@@ -11,12 +25,29 @@ def caesar_cipher(direction, received_text, shift_num):
         shift_num *= -1
 
     for char in received_text:
+        if not char.isalpha():
+            encrypted_text += char
+            continue
+
         try:
             encrypted_text += alphabet[alphabet.index(char) + shift_num]
         except IndexError:
             encrypted_text += alphabet[(alphabet.index(char) + shift_num - len(alphabet))]
 
+    return encrypted_text
 
-    print(encrypted_text)
+while True:
 
-caesar_cipher(direction, text, shift)
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    print(caesar_cipher(direction, text, shift))
+
+    end = input('Would you like to play again?'.lower())
+
+    if end == 'yes':
+        continue
+    else:
+        print('Goodbye')
+        break
